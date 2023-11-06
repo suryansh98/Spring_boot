aa4659ebf028af060a13f93174d7650cc0f33371
Suryansh Chaturvedi <suryansh.chaturvedi@stud.uni-heidelberg.de>
2023-11-06 13:41:15 +0100

    Set up spring boot with mysql

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,23 @@
+package com.luv2code.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    //define our init method
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes:-)";
+    }
+}
