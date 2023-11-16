ce0c8b89c17b6fcb3a0f9f16c64f65085653d1d2
Suryansh Chaturvedi <suryansh.chaturvedi@stud.uni-heidelberg.de>
2023-11-16 10:55:15 +0100

    JPQL using entityManager

---
+++ 03-spring-boot-hibernate-jpa-crud/03-cruddemo- Querying Objects with JPQL/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,52 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+
+    //define field for entity manager
+    private EntityManager entityManager;
+
+    //inject entity manager using constructor injection
+
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+
+    //implement save method
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+
+        entityManager.persist(theStudent);
+
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        //create Query //queries are for the entity Student and not the table student
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        //return result list
+        return theQuery.getResultList();
+    }
+
+
+}
